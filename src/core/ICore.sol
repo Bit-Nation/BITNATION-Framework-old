d5dc3938eec23fa6440d3bf597e0701937e578e0
DeveloppSoft <developpsoft@gmail.com>
2017-07-25 14:44:07 +0200

    add core interface

---
+++ src/core/ICore.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.13;
+
+
+/// @title ICore
+/// @author Eliott Teissonniere
+/// @dev Basic interface all cores need to implement
+interface ICore {
+    // Should only be called once, at setup time
+    function setup(address baseCoreAddr) public external;
+
+    // Authorization are managed by ds-auth, ds-group or ds-guard
+}
