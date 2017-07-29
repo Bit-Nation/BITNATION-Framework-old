76b46710711794c496dd4a2a482674aca7c30a05
DeveloppSoft <developpsoft@gmail.com>
2017-07-29 15:03:16 +0200

    add more tests

--- src/base/TestBase.sol
+++ src/base/TestBase.sol
@@ -14,19 +14,37 @@ contract FakeCore is ICore, BaseStorage {
     function didSetup() returns (bool) {
         return getStorage(sha3(0x1, 0x42)) == 1;
     }
+
+    function testThrow(uint toTest) {
+        require(1 != toTest);
+    }
+}
+
+contract Mock {
+    function shouldCrash() constant returns (bool) {
+        return true;
+    }
 }
 
 contract TestBase is DSTest {
     Base base;
-    FakeCore core;
 
     function setUp() {
-        core = new FakeCore();
-        base = new Base(address(core));
+        base = new Base(address(new FakeCore()));
     }
 
     function test_delegateIncomingCallsAndCheckSetupHasBeenDone() {
         FakeCore delegate = FakeCore(base);
         assert(delegate.didSetup() == true);
+    }
+
+    function testFail_functionThrow() {
+        FakeCore delegate = FakeCore(base);
+        delegate.testThrow(1);
+    }
+
+    function testFail_unknownFunction() {
+        Mock mock = Mock(base);
+        mock.shouldCrash();
     }
 }
