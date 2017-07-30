3613f9bc419ebe011f7b12e718592c1ea67b34c4
DeveloppSoft <developpsoft@gmail.com>
2017-07-30 16:38:08 +0200

    add IModule

---
+++ src/mods/IModule.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.13;
+
+import "../util/BaseStorage.sol";
+
+
+/// @title IModule
+/// @author Eliott Teissonniere
+/// @dev base contract all module should implement
+contract IModule is BaseStorage {
+    // empty, for the moment...
+}
